d9bc07a0a60b80611b03852af12b7afa386edcc8
Bahran Solomon tadesse <bahran.solomon@astu.edu.et>
2026-04-28 18:14:29 +0300

    show time and added history

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
+const history = JSON.parse(localStorage.getItem("town")) || [];
+
 async function getWeather() {
     const city = document.getElementById("userinput").value;
     
     if (!city ){
         alert("Enter a city to search")
         return}
-    try {
-
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=607d626a1af861486d45b070d3947bc3&units=metric`)
-        const data = await response.json();
+        try {
+            
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=607d626a1af861486d45b070d3947bc3&units=metric`)
+            const data = await response.json();
         console.log(data);
         
         if(data.cod === "404"){
-            document.getElementById("result").innerHTML = "<p>City not found. Try again.</p>"
+            document.getElementById("result").innerHTML = `<p>${data.message} Try again.</p>`
             return
         }
-        const condition = data.weather[0].id;
+        if(!history.includes(city)){
+            history.push(city)
+            if(history.length === 6){
+                history.shift()
+            }
+            
+        }
 
+        localStorage.setItem("town", JSON.stringify(history));
+        const historyList = document.getElementById("historyList");
+        historyList.innerHTML = "";
+        const stored = JSON.parse(localStorage.getItem("town")) || [];
+        
+        stored.forEach(element => {
+            const list = document.createElement("li");
+            list.textContent = element;
+            historyList.appendChild(list);
+        });
+        
+        
+        
+        
+        
+        
+
+        const condition = data.weather[0].id;
+        
         if(condition === 800){
             // document.body.style.backgroundColor = '#3084f1 ';
             document.body.classList.add('sunny-background')
@@ -27,6 +54,16 @@ async function getWeather() {
         if(condition >= 300 && condition <= 321 || condition >=500 && condition <= 531 ){
             document.body.classList.add('rainy-background')
         }
+        const timeCalculated = data.dt;
+        const shift = data.timezone;
+        const time = new Date((timeCalculated + shift) * 1000)
+        
+        const hours = time.getUTCHours().toString().padStart(2,"0")
+        const minutes = time.getUTCMinutes().toString().padStart(2,"0")
+        const day = time.toLocaleDateString("en-US",   
+             {  weekday: "long", 
+              timeZone: "UTC"
+             });
         document.getElementById("result").innerHTML = "<p>Loading...</p>";
         const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
@@ -38,7 +75,9 @@ async function getWeather() {
         <p>🌤️ Weather: ${data.weather[0].description}</p>
         <p>💧 Humidity: ${data.main.humidity}%</p>
         <p>💨 Wind: ${data.wind.speed} m/s</p>
+        <p>⌚ Time: ${day} ${hours}:${minutes} </p>
         `;
+        
         }
     catch (error) {
     console.error("Error:", error);
